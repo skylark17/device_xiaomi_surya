aea182a0d4edd6a29a17fe3d81807f0aceeb083b
skylark17 <axelancient1790@gmail.com>
2020-11-02 16:00:36 +0700

    Surya : bringup Pixel-Ify

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -7,10 +7,18 @@
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+#Gapps
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
@@ -20,4 +28,5 @@ BUILD_FINGERPRINT := POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.2.0.QJGMIX
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-HAVOC_BUILD_TYPE := Official
+TARGET_VENDOR_PRODUCT_NAME := surya
+
