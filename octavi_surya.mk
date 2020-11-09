8f2b33fab7dc20d20aaea1a3e73b5dbf010a6c69
skylark17 <axelancient1790@gmail.com>
2020-11-09 10:29:45 +0700

    surya : octavi-ify

--- octavi_surya.mk
+++ octavi_surya.mk
@@ -6,18 +6,22 @@
 
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Octavi-OS stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+
+# Official-ify
+OCTAVI_BUILDTYPE=Official
+OCTAVI_DEVICE_MAINTAINER := AgungPratama
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_surya
+PRODUCT_NAME := octavi_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := M2007J20CG
+PRODUCT_MODEL := POCO X3
 PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.3.0.QJGMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-HAVOC_BUILD_TYPE := Official
+
